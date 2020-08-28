42bb1001209b499752d17fe3c653e5afcf1ca1cb
David Chen <davidc3287@gmail.com>
2020-08-27 21:59:29 -0400

    add better file model handling

--- Codenames.py
+++ Codenames.py
@@ -8,18 +8,23 @@ Author: David Chen
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
+import pathlib
 
 
 class Codenames:
     def __init__(self):
         """ Load in Stanford GloVe vectors and initialize model """
-        # glove_file = '/home/dchen327/Downloads/GloVe/glove.6B.300d.txt'
-        # word2vec_glove_file = get_tmpfile('glove.6B.300d.word2vec.txt')
-        # glove2word2vec(glove_file, word2vec_glove_file)
-        # model = KeyedVectors.load_word2vec_format(word2vec_glove_file)
-        # model.save('glove_model.bin')
-
-        self.model = KeyedVectors.load('glove_model.bin')
+        model_file = pathlib.Path('glove_model.bin')
+        print('Gathering GloVe vectors...')
+        if model_file.exists():
+            self.model = KeyedVectors.load('glove_model.bin')
+        else:
+            print('Loading glove file from disk')
+            glove_file = '/home/dchen327/Downloads/GloVe/glove.6B.300d.txt'
+            word2vec_glove_file = get_tmpfile('glove.6B.300d.word2vec.txt')
+            glove2word2vec(glove_file, word2vec_glove_file)
+            self.model = KeyedVectors.load_word2vec_format(word2vec_glove_file)
+            self.model.save('glove_model.bin')
 
 
 if __name__ == "__main__":
